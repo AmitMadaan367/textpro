798c3f5860b363f613cdd65ff7952ee824272d1e
AmitMadaan367 <madaanamit367@gmail.com>
2022-05-26 10:07:17 +0530

    updated code

---
+++ after_install.sh
@@ -0,0 +1,13 @@
+# Install libaries
+cd /var/www/backend
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+# Set permission for all files
+sudo chown -R www-data:www-data /var/www/
+
+# Restart services
+sudo service apache2 restart
---
+++ before_install.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y python3 python-dev python3-pip apache2
+pip install --user --upgrade virtualenv
+sudo rm -rf /var/www/backend
