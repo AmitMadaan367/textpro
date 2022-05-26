345e1c91ae3f86645b1c074fdbb00bc8a33fa1b2
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-05-26 20:07:51 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -3,7 +3,9 @@ cd /var/www/html
 mkdir textpro
 
 sudo apt update
-sudo apt install python3-pip python3-dev apache2 libapache2-mod-wsgi-py3 -y
+sudo apt-get install python3-pip python3-dev apache2 -y
+sudo apt-get install libapache2-mod-wsgi-py3 -y
+
 pip install virtualenv
 virtualenv env
 source env/bin/activate
