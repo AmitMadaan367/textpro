295bf4a16a2ab05e133097a0d7ed2f32c7c1f2a6
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-05-26 19:44:53 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -5,7 +5,7 @@ mkdir textpro
 cd textpro
 
 sudo apt update
-sudo apt install python3-pip python3-dev apache2 -y
+sudo apt install python3-pip python3-dev apache2 libapache2-mod-wsgi-py3 -y
 pip install virtualenv
 virtualenv env
 source env/bin/activate
