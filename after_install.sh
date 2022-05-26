f772c40b21153ffd3fd5745b0f414837bbabd30d
AmitMadaan367 <madaanamit367@gmail.com>
2022-05-26 17:19:55 +0530

    updated code

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 # Restart services
+
+mv /home/ubuntu/apac.conf /etc/nginx/sites-available 
+
+sudo ln -s /etc/apache2/sites-available/apac.conf /etc/apache2/sites-enabled/
+
+sudo rm /etc/apache2/sites-enabled/000-default.conf
+
 sudo service apache2 restart
 
 
