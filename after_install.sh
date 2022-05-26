3d4570d2e130817ccc5a6f09608a499410ea5934
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-05-26 19:52:55 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -6,13 +6,13 @@ source env/bin/activate
 pip install -r requirements.txt
 
 
-# Restart services
+#deleting old conf file
 sudo rm /etc/apache2/sites-available/000-default.conf
 
 
 mv /var/www/html/textpro/000-default.conf /etc/apache2/sites-available 
 
-
+# Restart services
 sudo service apache2 restart
 
 
