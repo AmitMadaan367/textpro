9a7e52961ba6e8e2781206f2a0233ad95ff73ecd
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-06-01 12:34:23 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cd /var/www/html
+mkdir textpro
+cd /var/www/html/textpro
 apt update
 apt-get install python3-pip python3-dev apache2 libapache2-mod-wsgi-py3 python3-venv  -y
 python3 -m venv env
