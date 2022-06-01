0b0ace7bcc071f427320c2742ea243c98d5f34cb
AmitMadaan367 <55126789+AmitMadaan367@users.noreply.github.com>
2022-06-01 12:19:28 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /var/www/html
-mkdir textpro
 apt update
 apt-get install python3-pip python3-dev apache2 libapache2-mod-wsgi-py3 python3-venv  -y
 python3 -m venv env
 source env/bin/activate
+pip install gunicorn
